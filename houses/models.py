a29ef37cb4fe1731801621202c6b95c26d1cd195
Mercybett <merjep551@gmail.com>
2021-12-08 16:56:22 +0300

    create houses model

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.db.models.fields import SlugField
 
-# Create your models here.
+# Create your models here
+
+
+class House(models.Model):
+    class HomeType(models.TextChoices):
+        APARTMENT = 'Apartment'
+        BUNGALOW = 'Bungalow'
+        STUDIO = 'Studio'
+        TOWNHOUSE = 'Townhouse'
+        MAISONETTE = 'Maisonette'
+
+    class SaleType(models.TextChoices):
+        RENT = 'For Rent'
+        BUY = 'For Sale'
+
+    realtor = models.EmailField(max_length=255)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    county = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    home_type = models.CharField(max_length=50,
+                                 choices=HomeType.choices, default=HomeType.APARTMENT)
+    sale_type = models.CharField(
+        max_lenght=10, choices=SaleType.choices, default=SaleType.BUY)
+    main_photo = models.ImageField(upload_to='houses/')
+    photo_2 = models.ImageField(upload_to='houses/')
+    photo_3 = models.ImageField(upload_to='houses/')
+    photo_4 = models.ImageField(upload_to='houses/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
