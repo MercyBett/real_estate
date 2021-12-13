b3268874b0da9041e158cc53b7c18f346cd96d02
Mercybett <merjep551@gmail.com>
2021-12-13 12:16:15 +0300

    create post function for manage houses view

--- houses/views.py
+++ houses/views.py
@@ -1,5 +1,82 @@
-class ManageHousesView():
-  def get():
-    pass
-  def post(self):
-    pass
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import House
+
+
+class ManageHousesView(APIView):
+    def get(self, request, format=None):
+        pass
+
+    def post(self, request):
+        try:
+            user = request.user
+            if not user.is_realtor:
+                return Response(
+                    {'error': 'user does not have necessary permissions to create a house listing'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+            data = request.data
+            title = data['title']
+            slug = data['slug']
+            county = data['county']
+            location = data['location']
+            description = data['description']
+            price = data['price']
+            try:
+                price = int(price)
+            except:
+                return Response(
+                    {'error': 'price must be an integer'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            bedrooms = data['bedrooms']
+            try:
+                bedrooms = int(bedrooms)
+            except:
+                return Response(
+                    {'error': 'bedrooms must be an integer'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            bathrooms = data['bathrooms']
+            try:
+                bathrooms = float(bathrooms)
+            except:
+                return Response(
+                    {'error': 'bathrooms must be a decimal number'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if bathrooms <= 0 or bathrooms >= 10:
+                bathrooms = 1.0
+
+            bathrooms = round(bathrooms, 1)
+            home_type = data['home_type']
+            if home_type == 'APARTMENT':
+                home_type = 'Apartment'
+            elif home_type == 'BUNGALOW':
+                home_type = 'Bungalow'
+            elif home_type == 'STUDIO':
+                home_type = 'Studio'
+            elif home_type == 'TOWNHOUSE':
+                home_type = 'Townhouse'
+            else:
+                home_type = 'Maisonette'
+            sale_type = data['sale_type']
+            if sale_type == 'RENT':
+                sale_type = 'For Rent'
+            else:
+                sale_type = 'For Sale'
+            main_photo = data['main_photo']
+            photo_2 = data['photo_2']
+            photo_3 = data['photo_3']
+            photo_4 = data['photo_4']
+            is_published = data['is_published']
+            if is_published == 'True':
+                is_published = True
+            else:
+                is_published = False
+        except:
+            return Response(
+                {'error': 'something went wrong when retrieving houses'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
--- user/serializers.py
+++ user/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'name', 'is_realtor',)
