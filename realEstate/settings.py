8dfa78d105399b1ce964bb35436fd08498e6adee
Mercybett <merjep551@gmail.com>
2021-12-13 17:13:58 +0300

    fix SIMPLE_JWT typos in settings

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -152,8 +152,8 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
-    'AUTH_HEADER_TYPES': ('bearer',),
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.token.AccessToken'),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
