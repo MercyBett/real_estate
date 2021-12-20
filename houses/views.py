685d4be538f8386645e82e1fb4d54b799fd83a06
Mercybett <merjep551@gmail.com>
2021-12-20 23:04:56 +0300

    create a method to get data values

--- houses/views.py
+++ houses/views.py
@@ -41,6 +41,71 @@ class ManageHousesView(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    def get_values(self, data):
+        title = data['title']
+        slug = data['slug']
+        if House.objects.filter(slug=slug).exists():
+            return Response(
+                {'error': 'House with this slug already exists'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        county = data['county']
+        location = data['location']
+        description = data['description']
+        price = data['price']
+        try:
+            price = int(price)
+        except:
+            return Response(
+                {'error': 'price must be an integer'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        bedrooms = data['bedrooms']
+        try:
+            bedrooms = int(bedrooms)
+        except:
+            return Response(
+                {'error': 'bedrooms must be an integer'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        bathrooms = data['bathrooms']
+        try:
+            bathrooms = float(bathrooms)
+        except:
+            return Response(
+                {'error': 'bathrooms must be a decimal number'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if bathrooms <= 0 or bathrooms >= 10:
+            bathrooms = 1.0
+
+        bathrooms = round(bathrooms, 1)
+        home_type = data['home_type']
+        if home_type == 'APARTMENT':
+            home_type = 'Apartment'
+        elif home_type == 'BUNGALOW':
+            home_type = 'Bungalow'
+        elif home_type == 'STUDIO':
+            home_type = 'Studio'
+        elif home_type == 'TOWNHOUSE':
+            home_type = 'Townhouse'
+        else:
+            home_type = 'Maisonette'
+        sale_type = data['sale_type']
+        if sale_type == 'RENT':
+            sale_type = 'For Rent'
+        else:
+            sale_type = 'For Sale'
+        main_photo = data['main_photo']
+        photo_2 = data['photo_2']
+        photo_3 = data['photo_3']
+        photo_4 = data['photo_4']
+        is_published = data['is_published']
+        if is_published == 'True':
+            is_published = True
+        else:
+            is_published = False
+
     def post(self, request):
         try:
             user = request.user
@@ -50,69 +115,6 @@ class ManageHousesView(APIView):
                     status=status.HTTP_403_FORBIDDEN
                 )
             data = request.data
-            title = data['title']
-            slug = data['slug']
-            if House.objects.filter(slug=slug).exists():
-                return Response(
-                    {'error': 'House with this slug already exists'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            county = data['county']
-            location = data['location']
-            description = data['description']
-            price = data['price']
-            try:
-                price = int(price)
-            except:
-                return Response(
-                    {'error': 'price must be an integer'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            bedrooms = data['bedrooms']
-            try:
-                bedrooms = int(bedrooms)
-            except:
-                return Response(
-                    {'error': 'bedrooms must be an integer'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            bathrooms = data['bathrooms']
-            try:
-                bathrooms = float(bathrooms)
-            except:
-                return Response(
-                    {'error': 'bathrooms must be a decimal number'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            if bathrooms <= 0 or bathrooms >= 10:
-                bathrooms = 1.0
-
-            bathrooms = round(bathrooms, 1)
-            home_type = data['home_type']
-            if home_type == 'APARTMENT':
-                home_type = 'Apartment'
-            elif home_type == 'BUNGALOW':
-                home_type = 'Bungalow'
-            elif home_type == 'STUDIO':
-                home_type = 'Studio'
-            elif home_type == 'TOWNHOUSE':
-                home_type = 'Townhouse'
-            else:
-                home_type = 'Maisonette'
-            sale_type = data['sale_type']
-            if sale_type == 'RENT':
-                sale_type = 'For Rent'
-            else:
-                sale_type = 'For Sale'
-            main_photo = data['main_photo']
-            photo_2 = data['photo_2']
-            photo_3 = data['photo_3']
-            photo_4 = data['photo_4']
-            is_published = data['is_published']
-            if is_published == 'True':
-                is_published = True
-            else:
-                is_published = False
 
             House.objects.create(
                 realtor=user.email,
