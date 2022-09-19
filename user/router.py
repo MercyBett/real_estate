a7053e91e523f64843484b7bbbccca91e9b15354
Mercybett <merjep551@gmail.com>
2022-09-19 16:03:03 +0300

    fix:refactor the routers

--- user/router.py
+++ user/router.py
@@ -2,14 +2,10 @@ class AuthRouter:
     route_app_labels = {'user', 'admin', 'contenttypes', 'sessions'}
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return 'user'
-        return None
+        return 'user' if model._meta.app_label in self.route_app_labels else None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return 'user'
-        return None
+        return 'user' if model._meta.app_label in self.route_app_labels else None
 
     def allow_relation(self, obj1, obj2, **hints):
         if (
@@ -20,6 +16,4 @@ class AuthRouter:
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label in self.route_app_labels:
-            return db == 'user'
-        return None
+        return db == 'user' if app_label in self.route_app_labels else None
